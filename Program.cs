f7bae2250fcd88e6c489d8b344306b49657797b9
Christopher Hercules <chriherc215@gmail.com>
2024-10-08 14:32:51 -0600

    Christopher Hercules, 10/8/24, Lab 4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Hello, This is Mad Libs. This is Mad Libs. When you are dong providing the requested words");
+Console.WriteLine("\n I will build them into a funny story for you");
+Console.WriteLine("\n Enter the requested word types"); // new line \n
+Console.WriteLine();
+
+/*
+
+*/
+
+string orginalStory =
+// enter story
+string [] storyWords = orginalStory.Split(' ');
+
+for (int i =0; i<storyWords.Length; i++)
+{
+    Console.Write($"{storyWords[int]}");
+    for (int j=0; j<storyWords.Length; j++)
+}
