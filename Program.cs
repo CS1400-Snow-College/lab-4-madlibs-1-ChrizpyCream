04c3361cd8d114d1e92ea6edaa4a6c9f4d5a230a
Christopher Hercules <chriherc215@gmail.com>
2024-10-09 08:52:58 -0600

    2

--- Program.cs
+++ Program.cs
@@ -1,21 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
 
-Console.WriteLine("Hello, This is Mad Libs. This is Mad Libs. When you are dong providing the requested words");
-Console.WriteLine("\n I will build them into a funny story for you");
-Console.WriteLine("\n Enter the requested word types"); // new line \n
+Console.Write("Hello, This is Mad Libs. This is Mad Libs. When you are dong providing the requested words");
+Console.Write("\n I will build them into a funny story for you");
+Console.Write("\n Enter the requested word types"); // new line \n
 Console.WriteLine();
 
 /*
+"A vacation is when you take a trip to some (adjective) place with your (adjective) family. 
+Usually, you go to some place that is near a/an (noun) or up on a/an (noun). 
+A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun). 
+I like to spend my time (verb ending in “ing”) or (verb ending in “ing”). 
+When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, 
+and mothers sit around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body) My family is going to go to (place) 
+and I will practice (verb ending in “ing”) Parents need vacations more than kids because parents are always very (adjective) and because they have to work
+ (number) hours every day all year making enough (plural noun)  to pay for the vacation.";
+
+
+
+
+
 
 */
 
-string orginalStory =
-// enter story
-string [] storyWords = orginalStory.Split(' ');
-
-for (int i =0; i<storyWords.Length; i++)
-{
-    Console.Write($"{storyWords[int]}");
-    for (int j=0; j<storyWords.Length; j++)
-}
